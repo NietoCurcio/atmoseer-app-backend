1340a316d5835da20b86fafeafab79cf7032d76f
Felipe Curcio <felipe_nieto010@hotmail.com>
2024-03-23 00:47:01 -0300

    update config validation

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from enum import Enum
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import model_validator
 
-from app.Logger import logger
+from app.helpers.Logger import logger
 
 class ENVs(Enum):
     DEV = "dev"
@@ -20,12 +20,9 @@ class Settings(BaseSettings):
 
     @model_validator(mode="after")
     def _validate_env(self):
-        try:
-            assert self.ENV in [env.value for env in ENVs]
-        except AssertionError:
-            log = logger.get_logger(__name__)
-            log.error(f'Invalid ENV: {self.ENV}. Setting to "{ENVs.DEV.value}"')
-            self.ENV = ENVs.DEV.value
+        if self.ENV in ENVs: return self
+        logger.get_logger(__name__).error(f'Invalid ENV: {self.ENV}. Setting to "{ENVs.DEV.value}"')
+        self.ENV = ENVs.DEV.value
         return self
 
 settings = Settings()
