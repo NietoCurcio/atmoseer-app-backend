fb99a5bfe808cec7bb75be03ba129b61d30de026
Felipe Curcio <felipe_nieto010@hotmail.com>
2024-03-24 16:02:51 -0300

    update atmoseer_app_backend package initialization

--- atmoseer_app_backend/__init__.py
+++ atmoseer_app_backend/__init__.py
@@ -3,7 +3,7 @@ import os
 
 PATHS = [
     'atmoseer',
-    'atmoseer/src'
+    os.path.join('atmoseer', 'src'),
 ]
 
 def configure_paths(paths: list[str]):
@@ -11,8 +11,13 @@ def configure_paths(paths: list[str]):
         try:
             current_workdir = os.getcwd()
             path_to_add = os.path.join(current_workdir, path)
+            if not os.path.exists(path_to_add):
+                raise FileNotFoundError(
+                    f"Path {path_to_add} does not exist. Make sure to initialize {path} submodule"
+                )
             sys.path.append(path_to_add)
         except Exception as e:
             print(f"Error adding {path} to sys.path: {e}")
+            exit(1)
 
 configure_paths(PATHS)
