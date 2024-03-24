3bbc4f433f90d9ed5c8fccd42391adee5dc761f2
Felipe Curcio <felipe_nieto010@hotmail.com>
2024-03-24 17:51:56 -0300

    update reload setting

--- atmoseer_app_backend/main.py
+++ atmoseer_app_backend/main.py
@@ -1,7 +1,9 @@
 import uvicorn
 
+from atmoseer_app_backend.config import settings
+
 def main():
-    uvicorn.run("atmoseer_app_backend.app:app", port=3333, reload=True)
+    uvicorn.run("atmoseer_app_backend.app:app", port=3333, reload=settings.ENV == "dev")
 
 if __name__ == "__main__":
     main()
