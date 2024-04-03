62760b9c5323d411587914d38ffda2fe5dd58229
Felipe Curcio <felipe_nieto010@hotmail.com>
2024-04-03 17:32:31 -0300

    update Station model with updated csv

--- atmoseer_app_backend/helpers/GeoStationReader.py
+++ atmoseer_app_backend/helpers/GeoStationReader.py
@@ -1,8 +1,12 @@
 import csv
+from datetime import datetime
 from pathlib import Path
 from collections.abc import Generator
 
+from .Logger import logger
 from .models.Station import Station
+
+log = logger.get_logger(__name__)
 
 class GeoStationReader:
     def __init__(self, geo_stations_path: str) -> None:
@@ -13,23 +17,57 @@ class GeoStationReader:
         if not path.is_file(): raise FileNotFoundError(f"File '{path}' not found.")
         return path
 
+    def _create_station(self, row: list[str], ignore_first_column: bool) -> Station:
+        if ignore_first_column: row = row[1:]
+
+        # Pydantic maintains field order - https://docs.pydantic.dev/1.10/usage/models/#field-ordering
+        columns = Station.model_fields.keys()
+
+        assert len(columns) == len(row), "Number of columns and row do not match"
+
+        try:
+            row = map(str.strip, row)
+            station = dict(zip(columns, row))
+
+            station["latitude"] = float(station["latitude"])
+            station["longitude"] = float(station["longitude"])
+
+            station['altitude'] = (
+                float(station['altitude'].replace(",", "."))
+                if station['altitude'] else None
+            )
+            
+            station['start_date'] = (
+                datetime.strptime(station['start_date'], "%d/%m/%Y")
+                if station['start_date'] else None
+            )
+
+            return Station(**station)
+        except Exception as e:
+            log.error(f"Error creating Station: {e}")
+            raise e
+
     def csv_row_generator(
         self,
         file_path: str | None = None,
-        skip_header: bool = True
+        skip_header: bool = True,
+        ignore_first_column: bool = True
     ) -> Generator[Station, None, None]:
         file_path = file_path or self.geo_stations_path
-        with open(file_path, 'r', newline='') as csvfile:
+        try:
+            csvfile = open(file_path, 'r', newline='')
             csv_reader = csv.reader(csvfile)
 
-            if skip_header: next(csv_reader)
-            
-            for row in csv_reader:
-                station_name, latitude, longitude = row
-                yield Station(
-                    station_name=station_name,
-                    latitude=float(latitude),
-                    longitude=float(longitude)
-                )
+            header = next(csv_reader)
 
-geo_station_reader = GeoStationReader("./atmoseer_app_backend/helpers/stations.csv")
+            if not skip_header: yield self._create_station(header, ignore_first_column)
+
+            for row in csv_reader:
+                yield self._create_station(row, ignore_first_column)
+        except Exception as e:
+            log.error(f"Error reading CSV file '{file_path}': {e}")
+            raise e
+        finally:
+            csvfile.close()
+
+geo_station_reader = GeoStationReader("./atmoseer_app_backend/helpers/WeatherStations.csv")
--- atmoseer_app_backend/helpers/models/Station.py
+++ atmoseer_app_backend/helpers/models/Station.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class Station(BaseModel):
-    station_name: str
+    name: str
+    state: str
+    situation: str
     latitude: float
     longitude: float
+    altitude: float | None = None
+    start_date: datetime | None = None
+    station_id: str
