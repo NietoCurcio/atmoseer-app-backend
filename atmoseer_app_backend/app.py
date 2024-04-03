0d2c9c681dbe3f36755c968a30d5f86dad19bcd9
Felipe Curcio <felipe_nieto010@hotmail.com>
2024-04-03 20:27:40 -0300

    CORSMiddleware

--- atmoseer_app_backend/app.py
+++ atmoseer_app_backend/app.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from atmoseer_app_backend.api.router import api_router
 from atmoseer_app_backend.services.exceptions import add_custom_exception_handler
 
-app = FastAPI(title="Atmoseer-app API", version="0.1.0")
+app = FastAPI(
+    title="Atmoseer-app API",
+    version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 add_custom_exception_handler(app)
 
